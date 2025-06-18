1fc1f92ec18f47011a8c504ed2957392b4ef47ab
fyukmdaa <128161822+FyukMdaa@users.noreply.github.com>
2025-06-18 14:08:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-//今日の日付データを変数に格納
-//変数は"today"とする
-var today=new Date(); 
+<script>
+  document.getElementById("showtoday").innerHTML = showtoday();
+  function showtoday() {
+    const now = new Date();
+    const month = now.getMonth() + 1;
+    const date = now.getDate();
+    const showtoday = month + "月" + date + "日";
+    return showtoday;
+  }
+</script>
 
-//年・月・日・曜日を取得
-var year = today.getFullYear();
-var month = today.getMonth()+1;
-var week = today.getDay();
-var day = today.getDate();
-
-var week_ja= new Array("日","月","火","水","木","金","土");
